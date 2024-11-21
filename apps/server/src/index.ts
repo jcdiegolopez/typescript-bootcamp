29d467d58ffd5a8acb8117857118a2c560c17c38
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2024-11-21 13:02:51 -0600

    feat: init server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,15 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
+import { PrismaClient } from '../../../packages/database/node_modules/@prisma/client'
 
 const app = express()
+const client = new PrismaClient();
+
+app.get('/', async (_, res) => {
+  const result = await client.product.findMany();
+  return res.json({result})
+});
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
