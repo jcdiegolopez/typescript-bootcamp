c32a66ce5e47405d848adfcd5fd4a0bf17d96ccf
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2024-11-25 00:46:12 -0600

    feat: product request

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,23 +1,18 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
-import { PrismaClient } from '../../../packages/database/node_modules/@prisma/client'
+import productRouter from './routes/productRoutes'
+
 
 const app = express()
-const client = new PrismaClient();
 
-app.get('/', async (_, res) => {
-  const result = await client.product.findMany();
-  return res.json({result})
-});
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (_, res) => {
-  return res.json({ ok: true })
-})
+app.use('/api', productRouter)
 
 const port = process.env.PORT || 5001
 
