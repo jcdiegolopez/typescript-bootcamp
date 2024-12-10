50f0f7e5f496d9b34b1805ddb46d94d9e28c558a
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2024-12-10 12:46:22 -0600

    feat: realease/test

--- apps/server/.eslintrc.cjs
+++ apps/server/.eslintrc.cjs
@@ -1,8 +1,31 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['@repo/eslint-config/index.js'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      {
+        allowConstantExport: true,
+      },
+    ],
+  },
+  overrides: {
+    "files": ["./src/**/*.{test,spec}.{ts, js,tsx,jsx}"],
+    "env": {
+      "jest": true
+    },
+  }
 }
